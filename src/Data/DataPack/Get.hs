e708d5c673ba708d56e4eee12be5fc3f01690bd7
david-sledge <sledged@gmail.com>
2018-03-31 16:33:05 -0600

    Wonderful testing

--- src/Data/DataPack/Get.hs
+++ src/Data/DataPack/Get.hs
@@ -17,7 +17,7 @@
 
 module Data.DataPack.Get where
 
-import Prelude hiding (take)
+import Prelude hiding (take, map)
 import Control.Exception.Safe
 import Control.Monad.Identity
 import Control.Monad.Trans.Class
@@ -109,19 +109,25 @@ class (Monad m, Show p) => Stream s m p | s -> p where
   getPos :: s -> m p
 
 data FormatException =
-    Unused
-  | QualifiedNameException
-  | InvalidStateException
+    UnusedException
+  | ValueException
+  | NamespaceException
+  | ClassnameException
   | LocalNameException
-  | ClassnameNotAllowedException
+  | CollectionEndException
+  | InvalidStateException
+  | OptionException FormatException FormatException
   deriving (Show, Typeable, Eq, Ord)
 
 instance Exception FormatException where
-  displayException Unused = "Unused byte format"
-  displayException QualifiedNameException = "namespace or local name expected"
+  displayException UnusedException = "Unused byte format"
+  displayException ValueException = "value expected"
+  displayException NamespaceException = "namespace expected"
+  displayException ClassnameException = "classname expected"
+  displayException LocalNameException = "local name (string) expected"
+  displayException CollectionEndException = "collection end expected"
   displayException InvalidStateException = "invalid state"
-  displayException LocalNameException = "local name expected"
-  displayException ClassnameNotAllowedException = "classname not allowed here"
+  displayException (OptionException e1 e2) = displayException e1 ++ " or " ++ displayException e2
 
 data DPException = FormatException FormatException
   | ByteStreamException SomeException
@@ -167,7 +173,6 @@ data StateStack = Arr
   | LocalName
   deriving (Show, Eq, Ord)
 
---readString :: Int -> C.ByteString -> StateT [StateStack] (StateT s m) C.ByteString
 readString n byteString =
   let len = C.length byteString in
   if len < n
@@ -178,31 +183,195 @@ readString n byteString =
 
 readStream len = readString len C.empty
 
---valueEnd :: StateT [StateStack] (StateT s m) ()
---valueEnd =
+data StateProcessor m = StateProcessor
+  { empty :: m ()
+  , arr :: m ()
+  , map :: m ()
+  , obj :: m ()
+  , arrInit :: m ()
+  , objInit :: m ()
+  , clsNm :: m ()
+  , entryVal :: m ()
+  , loclNm :: m ()
+}
 
-validateNonText :: (MonadThrow m, MonadCatch m, Stream s m p) =>
- StateT [StateStack] (StateT s m) ()
-validateNonText = do
+processState handler = do
   ss <- get
   case ss of
-    [] -> pure ()
+    [] -> empty handler
     state:ss' ->
       case state of
-        Arr -> pure ()
-        Mp -> pure ()
-        Obj -> throwM $ FormatException QualifiedNameException
+        Arr -> arr handler
+        Mp -> map handler
+        Obj -> obj handler
         ColInit ->
           case ss' of
             [] -> throwM $ FormatException InvalidStateException
-            state':ss'' ->
+            state':_ ->
               case state' of
-                Obj -> throwM $ FormatException QualifiedNameException
-                Arr -> put $ state:ss''
+                Arr -> arrInit handler <* put ss'
+                Obj -> objInit handler <* put ss'
                 _ -> throwM $ FormatException InvalidStateException
-        ClsNm -> throwM $ FormatException QualifiedNameException
-        EntryValue -> pure ()
-        LocalName -> throwM $ FormatException LocalNameException
+        ClsNm -> clsNm handler
+        EntryValue -> entryVal handler
+        LocalName -> loclNm handler
+
+data FormatValidator m = FormatValidator
+  { valInColV :: m ()
+  , valOnlyV :: m ()
+  , clsNmV :: m ()
+  , objV :: m ()
+  , arrInitV :: m ()
+  , objInitV :: m ()
+  , loclNmV :: m ()
+}
+
+formatToState fv = StateProcessor
+  { empty = valOnlyV fv
+  , arr = valInColV fv
+  , map = valInColV fv
+  , obj = objV fv
+  , arrInit = arrInitV fv
+  , objInit = objInitV fv
+  , clsNm = clsNmV fv
+  , entryVal = valOnlyV fv
+  , loclNm = loclNmV fv
+}
+
+defaultValidator :: (MonadThrow m) => FormatValidator m
+defaultValidator = FormatValidator
+  { valInColV = throwM . FormatException $ OptionException ValueException CollectionEndException
+  , valOnlyV = throwM $ FormatException ValueException
+  , clsNmV = throwM . FormatException $ OptionException NamespaceException LocalNameException
+  , objV = throwM . FormatException . OptionException CollectionEndException $ OptionException NamespaceException LocalNameException
+  , arrInitV = throwM . FormatException . OptionException CollectionEndException $ OptionException ValueException ClassnameException
+  , objInitV = throwM . FormatException . OptionException (OptionException ClassnameException CollectionEndException) $ OptionException NamespaceException LocalNameException
+  , loclNmV = throwM $ FormatException LocalNameException
+}
+
+validateNonText :: (MonadThrow m) => StateT [StateStack] m ()
+validateNonText = processState . formatToState
+  $ defaultValidator {valOnlyV = pure (), valInColV = pure (), arrInitV = pure ()}
+
+validateString :: (MonadThrow m) => StateT [StateStack] m ()
+validateString = processState . formatToState
+  $ FormatValidator
+    { valInColV = pure ()
+    , valOnlyV = pure ()
+    , clsNmV = pure ()
+    , objV = pure ()
+    , arrInitV = pure ()
+    , objInitV = pure ()
+    , loclNmV = pure ()
+  }
+
+validateNamespace :: (MonadThrow m) => StateT [StateStack] m ()
+validateNamespace = processState . formatToState
+  $ defaultValidator
+    { clsNmV = pure ()
+    , objV = pure ()
+    , objInitV = pure ()
+  }
+
+validateClassname :: (MonadThrow m) => StateT [StateStack] m ()
+validateClassname = processState . formatToState
+  $ defaultValidator
+    { arrInitV = pure ()
+    , objInitV = pure ()
+  }
+
+validateColEnd :: (MonadThrow m) => StateT [StateStack] m ()
+validateColEnd = processState . formatToState
+  $ defaultValidator
+    { valInColV = pure ()
+    , objV = pure ()
+    , arrInitV = pure ()
+    , objInitV = pure ()
+  }
+
+defaultTransition :: (MonadThrow m) => StateProcessor m
+defaultTransition = StateProcessor
+  { empty = throwM $ FormatException InvalidStateException
+  , arr = throwM $ FormatException InvalidStateException
+  , map = throwM $ FormatException InvalidStateException
+  , obj = throwM $ FormatException InvalidStateException
+  , arrInit = throwM $ FormatException InvalidStateException
+  , objInit = throwM $ FormatException InvalidStateException
+  , clsNm = throwM $ FormatException InvalidStateException
+  , entryVal = throwM $ FormatException InvalidStateException
+  , loclNm = throwM $ FormatException InvalidStateException
+}
+
+transitionNonText :: (MonadThrow m) => StateT [StateStack] m ()
+transitionNonText = processState $ defaultTransition
+  { empty = pure ()
+  , arr = pure ()
+  , map = modify $ (:) EntryValue
+  , entryVal = modify tail
+}
+
+transitionColEnd :: (MonadThrow m) => StateT [StateStack] m ()
+transitionColEnd = processState $ defaultTransition
+  { arr = modify tail
+  , map = modify tail
+  , obj = modify tail
+}
+
+transitionString :: (MonadThrow m) => StateT [StateStack] m ()
+transitionString = processState $ defaultTransition
+  { empty = pure ()
+  , arr = pure ()
+  , map = modify $ (:) EntryValue
+  , obj = modify $ (:) EntryValue
+  , clsNm = modify tail
+  , entryVal = modify tail
+  , loclNm = do
+    modify tail
+    ss <- get
+    case ss of
+      [] -> throwM $ FormatException InvalidStateException
+      s:ss' ->
+        case s of
+        Obj -> modify $ (:) EntryValue
+        ClsNm -> modify tail
+        _ -> pure ()
+}
+
+transitionNamespace :: (MonadThrow m) => StateT [StateStack] m ()
+transitionNamespace = processState $ defaultTransition
+  { obj = modify $ (:) LocalName
+  , clsNm = modify $ (:) LocalName
+}
+
+transitionClassname :: (MonadThrow m) => StateT [StateStack] m ()
+transitionClassname = processState $ defaultTransition
+  { arr = modify $ (:) ClsNm
+  , obj = modify $ (:) ClsNm
+}
+
+transitionArray :: (MonadThrow m) => StateT [StateStack] m ()
+transitionArray = processState $ defaultTransition
+  { empty = modify $ (:) ColInit . (:) Arr
+  , arr = modify $ (:) ColInit . (:) Arr
+  , map = modify $ (:) ColInit . (:) Arr
+  , entryVal = modify $ (:) ColInit . (:) Arr
+}
+
+transitionMap :: (MonadThrow m) => StateT [StateStack] m ()
+transitionMap = processState $ defaultTransition
+  { empty = modify $ (:) Mp
+  , arr = modify $ (:) Mp
+  , map = modify $ (:) Mp
+  , entryVal = modify $ (:) Mp
+}
+
+transitionObject :: (MonadThrow m) => StateT [StateStack] m ()
+transitionObject = processState $ defaultTransition
+  { empty = modify $ (:) ColInit . (:) Obj
+  , arr = modify $ (:) ColInit . (:) Obj
+  , map = modify $ (:) ColInit . (:) Obj
+  , entryVal = modify $ (:) ColInit . (:) Obj
+}
 
 readValue :: (MonadThrow m, MonadCatch m, Stream s m p) =>
   StateT [StateStack] (StateT s m) PackType
@@ -210,55 +379,46 @@ readValue = do
   byte <- catch (lift unconsS) $ throwM . ByteStreamException
   pHandler <- get
   case idFormat byte of
-    Fixint x -> validateNonText $> Int x
-    FNil -> validateNonText $> Nil
-    FColEnd -> validateNonText $> ColEnd
-    FBool b -> validateNonText $> Bool b
-    FUInt8 -> validateNonText *> (Int . fromIntegral <$> readBytes 1)
-    FUInt16 -> validateNonText *> (Int . fromIntegral <$> readBytes 2)
-    FUInt32 -> validateNonText *> (Int . fromIntegral <$> readBytes 4)
-    FUInt64 -> validateNonText *> (UInt <$> readBytes 8)
+    Fixint x -> validateNonText $> Int x <* transitionNonText
+    FNil -> validateNonText $> Nil <* transitionNonText
+    FColEnd -> validateColEnd $> ColEnd <* transitionColEnd
+    FBool b -> validateNonText $> Bool b <* transitionNonText
+    FUInt8 -> validateNonText *> (Int . fromIntegral <$> readBytes 1) <* transitionNonText
+    FUInt16 -> validateNonText *> (Int . fromIntegral <$> readBytes 2) <* transitionNonText
+    FUInt32 -> validateNonText *> (Int . fromIntegral <$> readBytes 4) <* transitionNonText
+    FUInt64 -> validateNonText *> (UInt <$> readBytes 8) <* transitionNonText
     FInt8 -> do
       validateNonText
       byte <- readBytes 1
-      pure . Int $ fromIntegral (fromIntegral byte :: Int8)
+      Int (fromIntegral (fromIntegral byte :: Int8)) <$ transitionNonText
     FInt16 -> do
       validateNonText
       word <- readBytes 2
-      pure . Int $ fromIntegral (fromIntegral word :: Int16)
+      Int (fromIntegral (fromIntegral word :: Int16)) <$ transitionNonText
     FInt32 -> do
       validateNonText
       word <- readBytes 4
-      pure . Int $ fromIntegral (fromIntegral word :: Int32)
+      Int (fromIntegral (fromIntegral word :: Int32)) <$ transitionNonText
     FInt64 -> do
       validateNonText
       word <- readBytes 8
-      pure . Int $ fromIntegral (fromIntegral word :: Int64)
-    FFloat32 -> validateNonText *> (Float . wordToFloat . fromIntegral <$> readBytes 4)
-    FFloat64 -> validateNonText *> (Double . wordToDouble <$> readBytes 8)
-    FBin8 -> validateNonText *> (Bin <$> ((fromIntegral <$> readBytes 1) >>= readStream))
-    FBin16 -> validateNonText *> (Bin <$> ((fromIntegral <$> readBytes 2) >>= readStream))
-    FBin32 -> validateNonText *> (Bin <$> ((fromIntegral <$> readBytes 4) >>= readStream))
-    FStr8 -> Str . decodeUtf8 <$> ((fromIntegral <$> readBytes 1) >>= readStream)
-    FStr16 -> Str . decodeUtf8 <$> ((fromIntegral <$> readBytes 2) >>= readStream)
-    FStr32 -> Str . decodeUtf8 <$> ((fromIntegral <$> readBytes 4) >>= readStream)
-    FNs8 -> NS . decodeUtf8 <$> ((fromIntegral <$> readBytes 1) >>= readStream)
-    FNs16 -> NS . decodeUtf8 <$> ((fromIntegral <$> readBytes 2) >>= readStream)
-    FNs32 -> NS . decodeUtf8 <$> ((fromIntegral <$> readBytes 4) >>= readStream)
-    --{-
-    FClassname -> do
-      ss <- get
-      case ss of
-        [] -> throwM $ FormatException InvalidStateException
-        state:ss' ->
-          case state of
-            ColInit -> put (ClsNm:ss') $> Classname
-            _ -> throwM $ FormatException ClassnameNotAllowedException
-    --}
-    FArray -> validateNonText *> modify ((:) Arr . (:) ColInit) $> Array
-    FMap -> validateNonText *> modify ((:) Mp) $> Map
-    FObj -> validateNonText *> modify ((:) Obj . (:) ColInit) $> Object
-    FFixbin len -> validateNonText *> (Bin <$> readStream len)
-    FFixstr len -> Str . decodeUtf8 <$> readStream len
-    FFixns len -> NS . decodeUtf8 <$> readStream len
-    FUnused -> throwM $ FormatException Unused
+      Int (fromIntegral (fromIntegral word :: Int64)) <$ transitionNonText
+    FFloat32 -> validateNonText *> (Float . wordToFloat . fromIntegral <$> readBytes 4) <* transitionNonText
+    FFloat64 -> validateNonText *> (Double . wordToDouble <$> readBytes 8) <* transitionNonText
+    FBin8 -> validateNonText *> (Bin <$> ((fromIntegral <$> readBytes 1) >>= readStream)) <* transitionNonText
+    FBin16 -> validateNonText *> (Bin <$> ((fromIntegral <$> readBytes 2) >>= readStream)) <* transitionNonText
+    FBin32 -> validateNonText *> (Bin <$> ((fromIntegral <$> readBytes 4) >>= readStream)) <* transitionNonText
+    FStr8 -> validateString *> (Str . decodeUtf8 <$> ((fromIntegral <$> readBytes 1) >>= readStream)) <* transitionString
+    FStr16 -> validateString *> (Str . decodeUtf8 <$> ((fromIntegral <$> readBytes 2) >>= readStream)) <* transitionString
+    FStr32 -> validateString *> (Str . decodeUtf8 <$> ((fromIntegral <$> readBytes 4) >>= readStream)) <* transitionString
+    FNs8 -> validateNamespace *> (NS . decodeUtf8 <$> ((fromIntegral <$> readBytes 1) >>= readStream)) <* transitionNamespace
+    FNs16 -> validateNamespace *> (NS . decodeUtf8 <$> ((fromIntegral <$> readBytes 2) >>= readStream)) <* transitionNamespace
+    FNs32 -> validateNamespace *> (NS . decodeUtf8 <$> ((fromIntegral <$> readBytes 4) >>= readStream)) <* transitionNamespace
+    FClassname -> validateClassname $> Classname <* transitionClassname
+    FArray -> validateNonText $> Array <* transitionArray
+    FMap -> validateNonText $> Map <* transitionMap
+    FObj -> validateNonText $> Object <* transitionObject
+    FFixbin len -> validateNonText *> (Bin <$> readStream len) <* transitionNonText
+    FFixstr len -> validateString *> (Str . decodeUtf8 <$> readStream len) <* transitionString
+    FFixns len -> validateNamespace *> (NS . decodeUtf8 <$> readStream len) <* transitionNamespace
+    FUnused -> throwM $ FormatException UnusedException
--- test/Spec.hs
+++ test/Spec.hs
@@ -5,12 +5,16 @@ import Control.Monad.Identity
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.Identity
 import Control.Monad.Trans.State
+import Data.Function (on)
 import Data.Word
 import qualified Data.ByteString as C
 import Test.HUnit
 
 import Data.DataPack
 import Data.DataPack.Get
+
+instance Eq SomeException where
+    (==) = (==) `on` displayException
 
 data StreamWithPosition = StreamWithPosition C.ByteString Int
   deriving (Show, Eq, Ord)
@@ -29,46 +33,37 @@ instance (Monad m, MonadThrow m) => Stream StreamWithPosition m Int where
       pure (body, StreamWithPosition tail $ pos + n)
   getPos (StreamWithPosition bstr pos) = pure pos
 
-testWrapper label exec assertion = TestLabel label (TestCase (
-  case exec of
-    Right a -> assertion a
-    Left e -> putStr "\n\t" *> print e
-  ))
+testWrapper label exec assertion = TestLabel label . TestCase $ assertion exec
 
 testReadNil = testWrapper "testReadNil" (
     runIdentityT . runStateT (runStateT readValue [])
     $ StreamWithPosition (C.pack [nilByte]) 0
     :: Either SomeException ((PackType, [StateStack]), StreamWithPosition))
-  $ assertEqual "testReadNil" ((Nil,[]),StreamWithPosition C.empty 1)
+  $ assertEqual "testReadNil" $ Right ((Nil,[]),StreamWithPosition C.empty 1)
 
--- testValidateNonText_ ss = print
---   (runIdentityT . runStateT (runStateT validateNonText ss)
---     $ StreamWithPosition C.empty 0
---     :: Either SomeException (((), [StateStack]), StreamWithPosition))
-
-testValidateNonText_ ss label ss' = testWrapper label (
+testValidateNonText_ ss label a = testWrapper label (
   runIdentityT . runStateT (runStateT validateNonText ss)
   $ StreamWithPosition C.empty 0
   :: Either SomeException (((), [StateStack]), StreamWithPosition))
-  $ assertEqual "testValidateNonText" (((),ss'),StreamWithPosition C.empty 0)
+  $ assertEqual "testValidateNonText" a
 
 
 testValidateNonText =
-  [ testValidateNonText_ [] "non text with empty stack" []
-  , testValidateNonText_ [Arr] "non text with Arr state" [Arr]
-  , testValidateNonText_ [Mp] "non text with Mp state" [Mp]
-  , testValidateNonText_ [Obj] "non text with Obj state" [Obj]
-  , testValidateNonText_ [ColInit] "non text with ColInit state" [ColInit]
-  , testValidateNonText_ [ClsNm] "non text with ClsNm state" [ClsNm]
-  , testValidateNonText_ [EntryValue] "non text with EntryValue state" [EntryValue]
-  , testValidateNonText_ [LocalName] "non text with LocalName state" [LocalName]
-  , testValidateNonText_ [ColInit, Obj] "non text with [ColInit, Obj] stack" [ColInit, Obj]
-  , testValidateNonText_ [ColInit, Arr] "non text with [ColInit, Arr] stack" [ColInit]
-  , testValidateNonText_ [ColInit, Mp] "non text with [ColInit, Mp] stack" [ColInit, Mp]
-  , testValidateNonText_ [ColInit, ColInit] "non text with [ColInit, ColInit] stack" [ColInit, ColInit]
-  , testValidateNonText_ [ColInit, ClsNm] "non text with [ColInit, ClsNm] stack" [ColInit, ClsNm]
-  , testValidateNonText_ [ColInit, EntryValue] "non text with [ColInit, EntryValue] stack" [ColInit, EntryValue]
-  , testValidateNonText_ [ColInit, LocalName] "non text with [ColInit, LocalName] stack" [ColInit, LocalName]
+  [ testValidateNonText_ [] "non text with empty stack" $ Right (((),[]),StreamWithPosition C.empty 0)
+  , testValidateNonText_ [Arr] "non text with Arr state" $ Right (((),[Arr]),StreamWithPosition C.empty 0)
+  , testValidateNonText_ [Mp] "non text with Mp state" $ Right (((),[Mp]),StreamWithPosition C.empty 0)
+  , testValidateNonText_ [Obj] "non text with Obj state" . Left . SomeException . FormatException . OptionException CollectionEndException $ OptionException NamespaceException LocalNameException
+  , testValidateNonText_ [ColInit] "non text with ColInit state" . Left . SomeException $ FormatException InvalidStateException
+  , testValidateNonText_ [ClsNm] "non text with ClsNm state" . Left . SomeException . FormatException $ OptionException NamespaceException LocalNameException
+  , testValidateNonText_ [EntryValue] "non text with EntryValue state" $ Right (((),[EntryValue]),StreamWithPosition C.empty 0)
+  , testValidateNonText_ [LocalName] "non text with LocalName state" . Left . SomeException $ FormatException LocalNameException
+  , testValidateNonText_ [ColInit, Obj] "non text with [ColInit, Obj] stack" . Left . SomeException . FormatException . OptionException (OptionException ClassnameException CollectionEndException) $ OptionException NamespaceException LocalNameException
+  , testValidateNonText_ [ColInit, Arr] "non text with [ColInit, Arr] stack" $ Right (((),[Arr]),StreamWithPosition C.empty 0)
+  , testValidateNonText_ [ColInit, Mp] "non text with [ColInit, Mp] stack" . Left . SomeException $ FormatException InvalidStateException
+  , testValidateNonText_ [ColInit, ColInit] "non text with [ColInit, ColInit] stack" . Left . SomeException $ FormatException InvalidStateException
+  , testValidateNonText_ [ColInit, ClsNm] "non text with [ColInit, ClsNm] stack" . Left . SomeException $ FormatException InvalidStateException
+  , testValidateNonText_ [ColInit, EntryValue] "non text with [ColInit, EntryValue] stack" . Left . SomeException $ FormatException InvalidStateException
+  , testValidateNonText_ [ColInit, LocalName] "non text with [ColInit, LocalName] stack" . Left . SomeException $ FormatException InvalidStateException
   ]
 
 main = runTestTT $ TestList (testReadNil:testValidateNonText)
